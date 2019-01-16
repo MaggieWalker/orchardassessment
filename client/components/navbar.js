b8aa78014e7367a8fdcf9d9efcf2064bf3c176f8
Maggie Walker <mags21walker@gmail.com>
2019-01-16 18:29:37 -0500

    Feature Add: Building Out Frontend

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,24 +7,14 @@ import {logout} from '../store'
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
     <h1>NYC Restaurant Inspection Results</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
+    <nav />
     <hr />
+    <h2>Where should my friend eat?</h2>
+    <ul>
+      <li>Thai restaurant 1</li>
+      <li>Thai restaurant 2</li>
+      <li>Thai restaurant 3</li>
+    </ul>
   </div>
 )
 
